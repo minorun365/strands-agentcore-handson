d9f79735a6b9d770aff0473dec8c5f9aea8086c7
みのるん <74597894+minorun365@users.noreply.github.com>
2025-08-02 05:30:03 +0000

    更新

--- 2_agentcore/3_frontend.py
+++ 2_agentcore/3_frontend.py
@@ -17,12 +17,13 @@ if prompt := st.chat_input("メッセージを入力してね"):
         st.markdown(prompt)
 
     with st.chat_message("assistant"):
-        response = agent_core.invoke_agent_runtime(
-            agentRuntimeArn=os.getenv("AGENT_RUNTIME_ARN"),
-            payload=json.dumps({"input": {"prompt": prompt}}),
-            qualifier="DEFAULT"
-        )
-
-        response_body = response["response"].read()
-        response_data = json.loads(response_body)
-        st.write(response_data["result"]["content"]["text"])
+        with st.spinner("考え中…"):
+            response = agent_core.invoke_agent_runtime(
+                agentRuntimeArn=os.getenv("AGENT_RUNTIME_ARN"),
+                payload=json.dumps({"prompt": prompt}),
+                qualifier="DEFAULT"
+            )
+            response_body = response["response"].read()
+            response_data = json.loads(response_body.decode('utf-8'))
+        
+        st.write(response_data["result"]["content"][0]["text"])
